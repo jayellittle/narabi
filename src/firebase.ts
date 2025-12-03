32518e02799553a6c7d5dc7de2db10d63f0a8bce
Woo Sung Jahng <jlittle0919@icloud.com>
2025-12-04 02:08:37 +0900

    chore: remove Firebase emulator connection imports from Firebase initialization.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, connectAuthEmulator } from 'firebase/auth'
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
-import { getFunctions, connectFunctionsEmulator } from 'firebase/functions'
-import { getStorage, connectStorageEmulator } from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getFunctions } from 'firebase/functions'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
